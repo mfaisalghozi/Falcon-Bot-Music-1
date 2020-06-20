2ddefff2c0712b572927201d6a84864e8dccbe8d
mfaisalghozi <mrafgaming@gmail.com>
2020-06-20 16:19:53 +0700

    menambahkan file

---
+++ commands/help.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setAuthor('List of Music Command', 'https://media.discordapp.net/attachments/722032031810191400/723794646219751454/fed36e1bef3f18da4ef2546b5fdb3b79.png?width=560&height=560')
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A");
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed);
+  }
+};
